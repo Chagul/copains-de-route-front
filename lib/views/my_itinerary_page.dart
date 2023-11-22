4602f6b67bc70dd3a345c5e0e187e39ac0bea968
aurelien.plancke <aurelien.plancke.etu@univ-lille.fr>
2023-11-22 23:36:44 +0100

    Added navigation bar behavior and visual. Added navigation back and forth for homepage

---
+++ lib/views/my_itinerary_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class MyIntineraryPage extends StatelessWidget {
+  const MyIntineraryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Mes itinéraires"),
+    );
+  }
+}
