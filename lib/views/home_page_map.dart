fb1cf6aa443c5d382b7f944ad03da5779bbf203d
Aurelien Plancke <aurelien.plancke@hotmail.fr>
2023-11-23 23:58:25 +0100

    Added map

--- lib/views/home_page_map.dart
+++ lib/views/home_page_map.dart
@@ -1,23 +1,23 @@
+import 'package:copains_de_route/views/mapSample.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePageMap extends StatelessWidget {
   final Function changeView;
   const HomePageMap({super.key, required this.changeView});
 
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
   @override
   Widget build(BuildContext context) {
+    Permission.location.request();
+
+// You can also directly ask permission about its status.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map home'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            changeView();
-          },
-          child: const Text('Map home'),
+        appBar: AppBar(
+          title: const Text('Map home'),
         ),
-      ),
-    );
+        body: MapSample());
   }
 }
