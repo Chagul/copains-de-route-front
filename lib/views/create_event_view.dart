c3207818336fb98b6b157b90f26d4909f1b626fe
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-11-23 13:57:02 +0100

    added date picker but needs a bit of style

--- lib/views/create_event_view.dart
+++ lib/views/create_event_view.dart
@@ -1,9 +1,18 @@
 import 'package:copains_de_route/theme/custom_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
-class CreateEventView extends StatelessWidget {
+class CreateEventView extends StatefulWidget {
   const CreateEventView({super.key});
+
+  State<StatefulWidget> createState() {
+    return _CreateEventViewState();
+  }
+}
+
+class _CreateEventViewState extends State<CreateEventView> {
+  TextEditingController eventDateInput = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +28,15 @@ class CreateEventView extends StatelessWidget {
                   children: [
                     _buildCategoryTitle("Détails de l'événement"),
                     _buildTextInputField("Nom de l'événement", 0, 0),
-                    const Column(
+                    Column(
                       children: [
                         // calendar makes it a stateful widget https://www.fluttercampus.com/guide/39/how-to-show-date-picker-on-textfield-tap-and-get-formatted-date/
+                        _buildDatePicker("Date", 0, 0)
                         // time picker makes it a stateful widget https://api.flutter.dev/flutter/material/showTimePicker.html
                       ],
                     ),
                     _buildTextInputField("Description", 0, 0),
-                    _buildTextInputField(
+                    _buildNumberInputField(
                         "Nombre de participants maximum", 0, 0),
                     const Divider(
                       indent: 100,
@@ -89,5 +99,54 @@ class CreateEventView extends StatelessWidget {
         style: const TextStyle(color: CustomColorScheme.customOnSecondary),
       )
     ]);
+  }
+
+  Widget _buildNumberInputField(String name, int height, int width) {
+    return Column(children: [
+      Text(
+        name,
+        style: const TextStyle(color: Colors.black),
+      ),
+      TextField(
+        decoration: const InputDecoration(
+          filled: true,
+          fillColor: CustomColorScheme.customOnPrimary,
+        ),
+        style: const TextStyle(color: CustomColorScheme.customOnSecondary),
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+      )
+    ]);
+  }
+
+  Widget _buildDatePicker(String name, int height, int width) {
+    return Column(
+      children: [
+        Text(name, style: const TextStyle(color: Colors.black)),
+        TextField(
+          controller: eventDateInput,
+          decoration: const InputDecoration(icon: Icon(Icons.calendar_today)),
+          readOnly: true,
+          onTap: () async {
+            DateTime? eventDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now(),
+                lastDate: DateTime.now().add(const Duration(days: 365)),
+                barrierColor: CustomColorScheme.customPrimaryColor);
+
+            if (eventDate != null) {
+              String formattedDate = DateFormat('yyyy-MM-dd').format(eventDate);
+
+              setState(() {
+                eventDateInput.text = formattedDate;
+              });
+            }
+          },
+        )
+      ],
+    );
   }
 }
