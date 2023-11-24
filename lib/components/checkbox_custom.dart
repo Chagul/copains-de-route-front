b752b8ea1bc94bef72b162029ac36cff72ccb047
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-11-24 23:52:09 +0100

    widgets and style of the page is ok

---
+++ lib/components/checkbox_custom.dart
@@ -0,0 +1,57 @@
+import 'package:copains_de_route/theme/custom_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class CheckboxCustom extends StatefulWidget {
+  String name;
+  bool value;
+  int height;
+  int width;
+
+  CheckboxCustom(
+      {super.key,
+      required this.name,
+      required this.value,
+      required this.height,
+      required this.width});
+
+  @override
+  State<CheckboxCustom> createState() => _CheckboxCustomState();
+}
+
+class _CheckboxCustomState extends State<CheckboxCustom> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return CustomColorScheme.customOnSurface;
+      }
+      return CustomColorScheme.customOnPrimary;
+    }
+
+    widget.value = false;
+    return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return Row(
+        children: [
+          Checkbox(
+              value: widget.value,
+              checkColor: CustomColorScheme.customOnSecondary,
+              fillColor: MaterialStateProperty.resolveWith(getColor),
+              onChanged: (bool? change) {
+                setState(() {
+                  widget.value = change!;
+                });
+              }),
+          Text(widget.name, style: const TextStyle(color: Colors.black)),
+        ],
+      );
+    });
+  }
+}
--- lib/views/create_event_view.dart
+++ lib/views/create_event_view.dart
@@ -1,4 +1,5 @@
 import 'package:copains_de_route/theme/custom_color_scheme.dart';
+import 'package:copains_de_route/components/checkbox_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
@@ -22,7 +23,6 @@ class _CreateEventViewState extends State<CreateEventView> {
   bool isPavingStonesChecked = false;
   bool isOthersChecked = false;
   bool isPublic = false;
-  bool isPrivate = false;
   bool isBikeCity = false;
   bool isBikeAllTerrain = false;
   bool isBikeGravel = false;
@@ -35,73 +35,157 @@ class _CreateEventViewState extends State<CreateEventView> {
 
   Widget _buildBody(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
       child: Builder(
-          builder: (context) => Form(
-                child: Column(
-                  children: [
-                    _buildCategoryTitle("Détails de l'événement"),
-                    _buildTextInputField("Nom de l'événement", 0, 0),
-                    Column(
-                      children: [
-                        _buildDatePicker("Date", 0, 0),
-                        _buildTimePicker("Heure", 0, 0)
-                      ],
-                    ),
-                    _buildTextInputField("Description", 0, 0),
-                    _buildNumberInputField(
-                        "Nombre de participants maximum", 0, 0),
-                    const Divider(
-                      indent: 100,
-                      endIndent: 100,
-                      color: Colors.black,
-                    ),
-                    _buildCategoryTitle("Type de route"),
-                    Row(children: [
-                      Column(children: [
-                        _buildCheckbox("Terre", isDirtChecked, 0, 0),
-                        _buildCheckbox("Gravier", isGravelChecked, 0, 0),
-                        _buildCheckbox("Pavés", isPavingStonesChecked, 0, 0)
-                      ]),
-                      Column(children: [
-                        _buildCheckbox("Route", isRoadChecked, 0, 0),
-                        _buildCheckbox(
-                            "Piste cyclable", isCyclePathChecked, 0, 0),
-                        _buildCheckbox("Autres", isOthersChecked, 0, 0)
-                      ])
-                    ]),
-                    const Divider(
-                      indent: 100,
-                      endIndent: 100,
-                      color: Colors.black,
-                    ),
-                    _buildCategoryTitle("Visibilité"),
-                    Row(
-                      children: [
-                        _buildCheckbox("Public", isPublic, 0, 0),
-                        _buildCheckbox("Privé", isPrivate, 0, 0),
-                      ],
-                    ),
-                    const Divider(
-                      indent: 100,
-                      endIndent: 100,
-                      color: Colors.black,
-                    ),
-                    _buildCategoryTitle("Type de vélo"),
-                    Row(
-                      children: [
-                        Column(children: [
-                          _buildCheckbox("Ville", isBikeCity, 0, 0),
-                          _buildCheckbox("Gravel", isBikeGravel, 0, 0),
-                        ]),
-                        Column(children: [
-                          _buildCheckbox("BMX", isBikeBMX, 0, 0),
-                          _buildCheckbox(
-                              "Tout terrains", isBikeAllTerrain, 0, 0),
-                        ])
-                      ],
-                    ),
-                  ],
+          builder: (context) => SingleChildScrollView(
+                child: Form(
+                  child: Column(
+                    children: [
+                      _buildCategoryTitle("Détails de l'événement"),
+                      const SizedBox(height: 20),
+                      _buildTextInputField("Nom de l'événement", 1, 1),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(child: _buildDatePicker("Date", 0, 0)),
+                          Expanded(child: _buildTimePicker("Heure", 0, 0))
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTextInputField("Description", 5, 10),
+                      const SizedBox(height: 20),
+                      _buildNumberInputField(
+                          "Nombre de participants maximum", 0, 0),
+                      const Divider(
+                        indent: 100,
+                        endIndent: 100,
+                        color: Colors.black,
+                      ),
+                      _buildCategoryTitle("Type de route"),
+                      const SizedBox(height: 10),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CheckboxCustom(
+                                      name: "Piste cyclable",
+                                      value: isCyclePathChecked,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Gravier",
+                                      value: isGravelChecked,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Pavés",
+                                      value: isPavingStonesChecked,
+                                      height: 0,
+                                      width: 0)
+                                ]),
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CheckboxCustom(
+                                      name: "Route",
+                                      value: isRoadChecked,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Terre",
+                                      value: isDirtChecked,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Autres",
+                                      value: isOthersChecked,
+                                      height: 0,
+                                      width: 0)
+                                ])
+                          ]),
+                      const Divider(
+                        indent: 100,
+                        endIndent: 100,
+                        color: Colors.black,
+                      ),
+                      _buildCategoryTitle("Visibilité"),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [_buildSwitchInput("Privé", "Public")],
+                      ),
+                      const Divider(
+                        indent: 100,
+                        endIndent: 100,
+                        color: Colors.black,
+                      ),
+                      _buildCategoryTitle("Type de vélo"),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IntrinsicWidth(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CheckboxCustom(
+                                      name: "Tout terrains ",
+                                      value: isBikeCity,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Gravel",
+                                      value: isBikeGravel,
+                                      height: 0,
+                                      width: 0),
+                                ]),
+                          ),
+                          IntrinsicWidth(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CheckboxCustom(
+                                      name: "BMX  ",
+                                      value: isBikeBMX,
+                                      height: 0,
+                                      width: 0),
+                                  CheckboxCustom(
+                                      name: "Ville",
+                                      value: isBikeAllTerrain,
+                                      height: 0,
+                                      width: 0),
+                                ]),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: _cancel,
+                            // style
+                            child: const Text(
+                              "Annuler",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: _submitForm,
+                            // style
+                            child: const Text(
+                              "Confirmer",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               )),
     );
@@ -111,16 +195,20 @@ class _CreateEventViewState extends State<CreateEventView> {
     return Text(name,
         style: const TextStyle(
             color: CustomColorScheme.customPrimaryColor,
-            fontWeight: FontWeight.bold));
+            fontWeight: FontWeight.bold,
+            fontSize: 18));
   }
 
-  Widget _buildTextInputField(String name, int height, int width) {
-    return Column(children: [
+  Widget _buildTextInputField(String name, int minLines, int maxLines) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Text(
         name,
-        style: const TextStyle(color: Colors.black),
+        style:
+            const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
       ),
       TextFormField(
+        minLines: minLines,
+        maxLines: maxLines,
         decoration: const InputDecoration(
           filled: true,
           fillColor: CustomColorScheme.customOnPrimary,
@@ -134,73 +222,83 @@ class _CreateEventViewState extends State<CreateEventView> {
     return Column(children: [
       Text(
         name,
-        style: const TextStyle(color: Colors.black),
+        style:
+            const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
       ),
-      TextField(
-        decoration: const InputDecoration(
-          filled: true,
-          fillColor: CustomColorScheme.customOnPrimary,
+      SizedBox(
+        width: 70.0,
+        child: TextField(
+          decoration: const InputDecoration(
+            filled: true,
+            fillColor: CustomColorScheme.customOnPrimary,
+          ),
+          style: const TextStyle(color: CustomColorScheme.customOnSecondary),
+          keyboardType: TextInputType.number,
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.digitsOnly
+          ],
         ),
-        style: const TextStyle(color: CustomColorScheme.customOnSecondary),
-        keyboardType: TextInputType.number,
-        inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.digitsOnly
-        ],
       )
     ]);
   }
 
   Widget _buildDatePicker(String name, int height, int width) {
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(name, style: const TextStyle(color: Colors.black)),
-        TextField(
-          controller: eventDateInput,
-          decoration: const InputDecoration(
-              icon: Icon(Icons.calendar_today,
-                  color: CustomColorScheme.customOnSecondary)),
-          style: const TextStyle(color: CustomColorScheme.customOnSecondary),
-          readOnly: true,
-          onTap: () async {
-            DateTime? eventDate = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime.now(),
-                lastDate: DateTime.now().add(const Duration(days: 365)),
-                barrierColor: CustomColorScheme.customBackground,
-                builder: (context, child) {
-                  return Theme(
-                    data: Theme.of(context).copyWith(
-                      colorScheme: const ColorScheme.light(
-                        primary: CustomColorScheme.customSecondaryColor,
-                        onPrimary: CustomColorScheme.customOnSecondary,
-                        onSurface: CustomColorScheme.customOnSecondary,
-                      ),
-                      textButtonTheme: TextButtonThemeData(
-                        style: TextButton.styleFrom(
-                          foregroundColor: CustomColorScheme
-                              .customOnSecondary, // button text color
+        const SizedBox(width: 20.0),
+        SizedBox(
+          width: 140,
+          child: TextField(
+            controller: eventDateInput,
+            decoration: const InputDecoration(
+                icon: Icon(Icons.calendar_today,
+                    color: CustomColorScheme.customOnSecondary)),
+            style: const TextStyle(color: CustomColorScheme.customOnSecondary),
+            readOnly: true,
+            onTap: () async {
+              DateTime? eventDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime.now().add(const Duration(days: 365)),
+                  barrierColor: CustomColorScheme.customBackground,
+                  builder: (context, child) {
+                    return Theme(
+                      data: Theme.of(context).copyWith(
+                        colorScheme: const ColorScheme.light(
+                          primary: CustomColorScheme.customSecondaryColor,
+                          onPrimary: CustomColorScheme.customOnSecondary,
+                          onSurface: CustomColorScheme.customOnSecondary,
+                        ),
+                        textButtonTheme: TextButtonThemeData(
+                          style: TextButton.styleFrom(
+                            foregroundColor:
+                                CustomColorScheme.customOnSecondary,
+                          ),
                         ),
                       ),
-                    ),
-                    child: child!,
-                  );
-                });
-            if (eventDate != null) {
-              String formattedDate = DateFormat('yyyy-MM-dd').format(eventDate);
+                      child: child!,
+                    );
+                  });
+              if (eventDate != null) {
+                String formattedDate =
+                    DateFormat('yyyy-MM-dd').format(eventDate);
 
-              setState(() {
-                eventDateInput.text = formattedDate;
-              });
-            }
-          },
+                setState(() {
+                  eventDateInput.text = formattedDate;
+                });
+              }
+            },
+          ),
         )
       ],
     );
   }
 
   Widget _buildTimePicker(String name, int height, int width) {
-    return Row(children: <Widget>[
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
       ElevatedButton(
           child: Text(name, style: const TextStyle(color: Colors.black)),
           onPressed: () async {
@@ -232,6 +330,7 @@ class _CreateEventViewState extends State<CreateEventView> {
               });
             }
           }),
+      const SizedBox(width: 20.0),
       if (eventTime != null)
         Text(formatTimeOfDay(eventTime),
             style: const TextStyle(
@@ -240,38 +339,29 @@ class _CreateEventViewState extends State<CreateEventView> {
     ]);
   }
 
-  Widget _buildCheckbox(String name, bool value, int height, int width) {
-    return Row(
-      children: [
-        Text(name, style: const TextStyle(color: Colors.black)),
-        Checkbox(
-            value: value,
-            checkColor: Colors.red,
-            fillColor: MaterialStateProperty.resolveWith(getCheckboxColor),
-            onChanged: (bool? change) {
-              setState(() {
-                value = change!;
-              });
-            }),
-      ],
-    );
+  Widget _buildSwitchInput(String name1, String name2) {
+    return Row(children: [
+      Text(name1, style: const TextStyle(color: Colors.black)),
+      Switch(
+        value: isPublic,
+        activeColor: CustomColorScheme.customPrimaryColor,
+        onChanged: (bool value) {
+          setState(() {
+            isPublic = value;
+          });
+        },
+      ),
+      Text(name2, style: const TextStyle(color: Colors.black)),
+    ]);
   }
+
+  void _submitForm() {}
+
+  void _cancel() {}
 
   String formatTimeOfDay(TimeOfDay? t) {
     final now = DateTime.now();
     final dt = DateTime(now.year, now.month, now.day, t!.hour, t.minute);
     return DateFormat("HH:mm").format(dt);
-  }
-
-  Color getCheckboxColor(Set<MaterialState> states) {
-    const Set<MaterialState> interactiveStates = <MaterialState>{
-      MaterialState.pressed,
-      MaterialState.hovered,
-      MaterialState.focused,
-    };
-    if (states.any(interactiveStates.contains)) {
-      return CustomColorScheme.customSecondaryColor;
-    }
-    return CustomColorScheme.customPrimaryColor;
   }
 }
