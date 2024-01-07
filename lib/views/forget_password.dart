56050c062624d053c8eb2a49b79cfef9e9472ba0
Kesraoui <kesraoui.nassima2017@gmail.com>
2024-01-07 01:49:12 +0100

    change from a blocprovider to blocbuilder

--- lib/views/forget_password.dart
+++ lib/views/forget_password.dart
@@ -1,22 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:copains_de_route/components/login_screen/email_widget.dart';
 import 'package:copains_de_route/cubit/login/login_cubit.dart';
 import 'package:copains_de_route/cubit/login/login_state.dart';
 import 'package:copains_de_route/theme/custom_color_scheme.dart';
 import 'package:copains_de_route/views/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LoginCubit(
-        UserInitialState(),
-      ),
-      child: _ForgotPasswordContent(),
-    );
+    return _ForgotPasswordContent();
   }
 }
 
@@ -33,42 +28,50 @@ class _ForgotPasswordContentState extends State<_ForgotPasswordContent> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColorScheme.customPrimaryColor,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            const SizedBox(height: 50),
-            Container(
-              width: 230.0,
-              height: 250.0,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/logo_copains_de_route_withoutbg.png'),
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                      "Entrez votre e-mail pour la récupération de votre mot de passe :",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
+      body: BlocBuilder<LoginCubit, LoginState>(
+        builder: (context, state) {
+          if (state is ResetPasswordLinkSentState) {
+            _showSnackBar(context);
+          }
+
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                const SizedBox(height: 50),
+                Container(
+                  width: 230.0,
+                  height: 250.0,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/logo_copains_de_route_withoutbg.png'),
+                      fit: BoxFit.contain,
                     ),
-                    const SizedBox(height: 20),
-                    EmailWidget(controller: emailController),
-                    const SizedBox(height: 20),
-                    _submitButton(context),
-                  ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          "Entrez votre e-mail pour la récupération de votre mot de passe :",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                        const SizedBox(height: 20),
+                        EmailWidget(controller: emailController),
+                        const SizedBox(height: 20),
+                        _submitButton(context),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -76,51 +79,49 @@ class _ForgotPasswordContentState extends State<_ForgotPasswordContent> {
   Widget _submitButton(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 20.0),
-      child: BlocConsumer<LoginCubit, LoginState>(
-        listener: (context, state) {
-          if (state is ResetPasswordLinkSentState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: const Text(
-                  'Un email vous a été envoyé pour réinitialiser votre mot de passe',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                backgroundColor: Colors.white.withOpacity(0.5),
-                duration: const Duration(seconds: 5),
-                behavior: SnackBarBehavior.floating,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                margin: const EdgeInsets.all(8.0),
-              ),
-            );
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const LoginScreen()),
-            );
-          } 
+      child: ElevatedButton(
+        onPressed: () {
+          if (formKey.currentState!.validate()) {
+            context.read<LoginCubit>().sendResetPasswordLink(emailController.text);
+          }
         },
-        builder: (context, state) {
-          return ElevatedButton(
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                context.read<LoginCubit>().sendResetPasswordLink(emailController.text); // Replace 'email' with your email variable
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: const Color(0xFFFDD856),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              minimumSize: const Size(400, 50),
-            ),
-            child: const Text('Réinitialiser le mot de passe'),
-          );
-        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: CustomColorScheme.customSecondaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          minimumSize: const Size(400, 50),
+        ),
+        child: const Text('Réinitialiser le mot de passe'),
       ),
     );
   }
+
+  void _showSnackBar(BuildContext context) {
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text(
+          'Un email vous a été envoyé pour réinitialiser votre mot de passe',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white.withOpacity(0.5),
+        duration: const Duration(seconds: 5),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        margin: const EdgeInsets.all(8.0),
+      ),
+    );
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  });
+}
+
 }
